0bd9ca5c8d338e0743c683185149743dc80af5ab
Eric.R.E <erycrazy@hotmail.com>
2026-02-10 16:45:50 -0300

    Add files via upload

---
+++ Exercicio003.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudosCS.Ex0006
+{
+    class Exercicio003
+    {
+        public string apresentar(string nomeParam, int idadeParam)
+        {
+            string mensagem = $"Olá meu nome é {nomeParam}, e eu tenho {idadeParam}";
+
+            return mensagem;
+        }
+
+        public static void Inicializar03()
+        {
+            var apresentando = new Exercicio003();
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            int.TryParse(Console.ReadLine(), out int idade);
+
+            Console.WriteLine(apresentando.apresentar(nome, idade));
+        }
+    }
+}
