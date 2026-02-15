12c4c37a2448410208d5d77cf5c017423d247631
Eric.R.E <erycrazy@hotmail.com>
2026-02-15 13:48:45 -0300

    Add files via upload

---
+++ Exercicio007.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace EstudosCS.Ex0006
+{
+    class ContaBancaria
+    {
+        private double Saldo;
+
+        public double GetSaldo()
+        {
+            return Saldo;
+        }
+
+        public void SetSaldo(double saldoParam)
+        {
+            Saldo = saldoParam;
+        }
+
+
+        }
+
+    class MostrarDados
+    {
+        public static void Inicializar07()
+        {
+            var dinheiro = new ContaBancaria();
+
+            dinheiro.SetSaldo(25.43);
+            Console.WriteLine(dinheiro.GetSaldo());
+        }
+    }
+}
