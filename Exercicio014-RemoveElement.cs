f8f7099a601501aef6a7dacd133b319e6b95d86a
Eric.R.E <erycrazy@hotmail.com>
2026-02-25 21:30:45 -0300

    Add files via upload

---
+++ Exercicio014-RemoveElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudosCS.Exercicios
+{
+    class Exercicio014_RemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {            
+                if (val != nums[i])
+                {
+                    nums[k] = nums[i]; 
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
+
